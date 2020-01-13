4deeec395c964a389a1bfdcbec509b03d4770645
Denys <denys.cheporniuk@gmail.com>
2020-01-13 20:22:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.328050d544382b217a12c817dcf12095.js"
 );
 
 self.addEventListener('message', (event) => {
